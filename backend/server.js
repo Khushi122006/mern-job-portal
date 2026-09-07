3a5cc77e4b62cb093f0d42d2bfbaebe66c3313fe
Khushi Pandey <khushi.pandey_cs24@gla.ac.in>
2026-09-08 04:12:30 +0530

    Update company name

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.get("/api/jobs", (req, res) => {
     res.json([
         {
             title: "Software Developer",
-            company: "TCS",
+            company: "Microsoft",
             location: "Noida"
         },
         {
